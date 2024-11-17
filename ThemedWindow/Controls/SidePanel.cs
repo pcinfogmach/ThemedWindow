dcd55e6c3ebdad337d9978b6d32efb7ae09e9b6f
pcinfogmach <pcinfoGmach@gmail.com>
2024-11-17 18:01:09 +0200

    sidepanel

---
+++ ThemedWindow/Controls/SidePanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ThemedWindow.Helpers;
+
+namespace ThemedWindow.Controls
+{
+    internal class SidePanel : TabControl
+    {
+        public ICommand ChangeViewCommand => new RelayObjectCommand(ChangeViewIndex);
+
+        void ChangeViewIndex(object parameter)
+        {
+            if (parameter is int index)   SelectedIndex = index;
+        }
+    }
+}
--- ThemedWindowDemo/ThemedWindowSample.xaml.cs
+++ ThemedWindowDemo/ThemedWindowSample.xaml.cs
@@ -24,45 +24,6 @@ namespace ThemedWindowDemo
         public ThemedWindowSample()
         {
             InitializeComponent();
-            textBox.Text = @"o populate a TextBlock in a C# WPF application, you can set the Text property of the TextBlock control. Here’s a simple example of how to do this:
-XAML:
-
-<Window x:Class=""WpfApp.MainWindow""
-        xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
-        xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
-        Title=""TextBlock Example"" Height=""200"" Width=""300"">
-    <Grid>
-        <TextBlock Name=""myTextBlock"" HorizontalAlignment=""Center"" VerticalAlignment=""Center"" FontSize=""16""/>
-    </Grid>
-</Window>
-
-C# (Code-Behind):
-
-using System.Windows;
-
-namespace WpfApp
-{
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-            PopulateTextBlock();
-        }
-
-        private void PopulateTextBlock()
-        {
-            myTextBlock.Text = ""Hello, WPF!"";
-        }
-    }
-}
-
-In this example:
-
-    The TextBlock is defined in the XAML with the name myTextBlock.
-    In the code-behind, the PopulateTextBlock() method sets the Text property to ""Hello, WPF!"". This populates the TextBlock with the desired text when the window is loaded.
-
-You can dynamically populate the TextBlock based on your application logic by updating the Text property at any time.";
         }
     }
 }
